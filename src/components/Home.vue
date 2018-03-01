d8e8e9c613a892ffc7c9e8211f33c449239feba2
farmatique <farmatique@gmail.com>
2018-03-01 16:56:35 +0200

    homepage carousel and buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,47 @@
 <template>
 	<div>
-		<p>Home page</p>
+		<v-layout row >
+			<v-carousel>
+		    <v-carousel-item 
+		    	v-for="meetup in meetups" 
+		    	:src="meetup.imageUrl" 
+		    	:key="meetup.id"
+		    >
+					<div class="title text-xs-center py-3">{{ meetup.title }}</div>
+	    	</v-carousel-item>
+		  </v-carousel>
+		</v-layout>
+		<v-layout row wrap class="mt-2">
+			<v-flex xs12 sm6 class="text-sm-right text-xs-center">
+				<v-btn large router to="/meetups/new" class="primary">Create New Meetup</v-btn>
+			</v-flex>
+			<v-flex xs12 sm6 class="text-sm-left text-xs-center">
+				<v-btn large router to="/meetups" class="info">Check Meetups</v-btn>
+			</v-flex>
+		</v-layout>
 	</div>
 </template>
+
+<script>
+	export default {
+		data(){
+			return{
+				meetups: [
+					{ imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/48/1201044_original_%281%29.jpg', id:'fgdgdsfsdf', title: 'Meetup at Dnipro' },
+					{ imageUrl: 'https://farm8.staticflickr.com/7387/13333327953_26bc37001e_k.jpg', id:'fgdgdsfsdfjjggc', title: 'Meetup at Kiev' }
+				]
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.title{
+		left: 0;
+		right: 0;
+		position: absolute;
+		bottom: 50px;
+		color: #fff;
+		background-color: rgba(0,0,0,.5);
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.teal.darken1,
+    secondary: colors.teal.lighten4,
+    accent: colors.teal.accent4,
+    info: colors.teal.lighten1
+  }
+})
 
 Vue.config.productionTip = false
 
